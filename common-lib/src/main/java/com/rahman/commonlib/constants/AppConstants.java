e4c8011dedeb2bd276240fe4ac4f4cd2a3c0dc2f
Aditya Rahman Sidqi <adityars.sidqi@gmail.com>
2025-07-23 15:23:41 +0700

    feat: initial commit

---
+++ common-lib/src/main/java/com/rahman/commonlib/constants/AppConstants.java
@@ -0,0 +1,11 @@
+package com.rahman.commonlib.constants;
+
+public class AppConstants {
+
+    private AppConstants() {
+        throw new IllegalStateException("AppConstants class");
+    }
+
+    public static final String DEFAULT_CURRENCY = "IDR";
+    public static final int DEFAULT_PAGE_SIZE = 10;
+}
