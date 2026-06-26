35b0dafe7d272a744967df39817e229ee3325bcf
Aditya Rahman Sidqi <adityars.sidqi@gmail.com>
2026-06-26 23:17:05 +0700

    feat: add auth-service route to api-gateway

--- services/api-gateway/src/test/java/com/takapedia/apigateway/AuthRouteTest.java
+++ services/api-gateway/src/test/java/com/takapedia/apigateway/AuthRouteTest.java
@@ -1,4 +1,68 @@
 package com.takapedia.apigateway;
 
-public class AuthRouteTest {
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AuthRouteTest {
+
+    static WireMockServer wireMock;
+
+    @LocalServerPort
+    int port;
+
+    WebTestClient webTestClient;
+
+    @BeforeAll
+    static void startWireMock() {
+        wireMock = new WireMockServer(options().dynamicPort());
+        wireMock.start();
+    }
+
+    @BeforeEach
+    void setupClient() {
+        webTestClient = WebTestClient.bindToServer()
+                .baseUrl("http://localhost:" + port)
+                .build();
+    }
+
+    @AfterAll
+    static void stopWireMock() {
+        wireMock.stop();
+    }
+
+    @DynamicPropertySource
+    static void registerAuthUrl(DynamicPropertyRegistry registry) {
+        registry.add("services.auth.url", () -> "http://localhost:" + wireMock.port());
+    }
+
+    @Test
+    void shouldRouteToAuthService() {
+        wireMock.stubFor(post(urlEqualTo("/api/v1/auth/login"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"token\":\"dummy-token\"}")));
+
+        webTestClient.post()
+                .uri("/api/v1/auth/login")
+                .header("Content-Type", "application/json")
+                .bodyValue("{\"email\":\"a@b.com\",\"password\":\"secret\"}")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.token").isEqualTo("dummy-token");
+    }
 }
